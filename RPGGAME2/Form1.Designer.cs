37b611d1707cb21f66f89be8b486ec45f9110a51
YazanAlAraaj <yazanxkinge@gmail.com>
2026-04-10 16:30:32 +0300

    Added button to start the game Added some basic game logic Added equipment menu

--- RPGGAME2/Form1.Designer.cs
+++ RPGGAME2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RPGGAME2
 {
-    partial class Form1
+    partial class rpggame1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,33 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.lblExperience = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblHitPoints = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblEquipment = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnStart
             // 
-            this.button1.Location = new System.Drawing.Point(447, 77);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnStart.Font = new System.Drawing.Font("Tahoma", 16F);
+            this.btnStart.Location = new System.Drawing.Point(347, 197);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(205, 97);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnTest_Click);
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 1);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gold: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Experience: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Level: ";
+            // 
+            // lblGold
+            // 
+            this.lblGold.AccessibleName = "lblGold";
+            this.lblGold.AutoSize = true;
+            this.lblGold.Location = new System.Drawing.Point(35, 1);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(13, 13);
+            this.lblGold.TabIndex = 7;
+            this.lblGold.Text = "0";
+            // 
+            // lblExperience
+            // 
+            this.lblExperience.AccessibleName = "lblExperience";
+            this.lblExperience.AutoSize = true;
+            this.lblExperience.Location = new System.Drawing.Point(67, 1);
+            this.lblExperience.Name = "lblExperience";
+            this.lblExperience.Size = new System.Drawing.Size(13, 13);
+            this.lblExperience.TabIndex = 8;
+            this.lblExperience.Text = "0";
+            this.lblExperience.Click += new System.EventHandler(this.lblExperience_Click);
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AccessibleName = "lblLevel";
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(48, 1);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(13, 13);
+            this.lblLevel.TabIndex = 9;
+            this.lblLevel.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hit Points: ";
+            // 
+            // lblHitPoints
+            // 
+            this.lblHitPoints.AccessibleName = "lblHitPoints";
+            this.lblHitPoints.AutoSize = true;
+            this.lblHitPoints.Location = new System.Drawing.Point(59, 3);
+            this.lblHitPoints.Name = "lblHitPoints";
+            this.lblHitPoints.Size = new System.Drawing.Size(13, 13);
+            this.lblHitPoints.TabIndex = 6;
+            this.lblHitPoints.Text = "0";
+            this.lblHitPoints.Click += new System.EventHandler(this.lblHitPoints_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lblExperience);
+            this.panel1.Location = new System.Drawing.Point(12, 68);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(94, 18);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lblLevel);
+            this.panel2.Location = new System.Drawing.Point(12, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(94, 18);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.lblHitPoints);
+            this.panel3.Location = new System.Drawing.Point(12, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(94, 18);
+            this.panel3.TabIndex = 13;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.lblGold);
+            this.panel4.Location = new System.Drawing.Point(12, 31);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(94, 18);
+            this.panel4.TabIndex = 12;
+            // 
+            // lblEquipment
+            // 
+            this.lblEquipment.AutoSize = true;
+            this.lblEquipment.Location = new System.Drawing.Point(16, 1);
+            this.lblEquipment.Name = "lblEquipment";
+            this.lblEquipment.Size = new System.Drawing.Size(57, 13);
+            this.lblEquipment.TabIndex = 15;
+            this.lblEquipment.Text = "Equipment";
+            this.lblEquipment.Click += new System.EventHandler(this.lblEquipment_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel5.Controls.Add(this.panel6);
+            this.panel5.Location = new System.Drawing.Point(12, 92);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(94, 372);
+            this.panel5.TabIndex = 16;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.lblEquipment);
+            this.panel6.Location = new System.Drawing.Point(-2, -2);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(94, 18);
+            this.panel6.TabIndex = 0;
+            // 
+            // rpggame1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(897, 503);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(902, 507);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "rpggame1";
+            this.Text = "RpgGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.Label lblExperience;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblHitPoints;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblEquipment;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
     }
 }
 
--- RPGGAME2/Form1.cs
+++ RPGGAME2/Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace RPGGAME2
 {
-    public partial class Form1 : Form
+    public partial class rpggame1 : Form
     {
-        public Form1()
+        public rpggame1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblHitPoints.Text = "100";
+            lblGold.Text = "100";
+            lblLevel.Text = "1";
+            lblExperience.Text = "0";
+            btnStart.Hide();
+        }
+
+        private void lblExperience_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHitPoints_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEquipment_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
