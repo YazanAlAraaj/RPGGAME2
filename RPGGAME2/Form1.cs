d9228a65d95aae9b56bc64d1ce008206d675113f
YazanAlAraaj <yazanxkinge@gmail.com>
2026-04-13 01:08:28 +0300

    Added classes (Player,Location,...etc) Learned how to create a class-level variable and created one Added some of the player components like HP , exp , gold and displayed them on the forms used private access modifier for the first time

--- RPGGAME2/Form1.cs
+++ RPGGAME2/Form1.cs
@@ -8,13 +8,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace RPGGAME2
 {
     public partial class rpggame1 : Form
     {
+
+        private Player _player = new Player();
+
         public rpggame1()
         {
             InitializeComponent();
+            
+            _player.MaxHp = 100;
+            _player.CurrentHp = 100;
+            _player.Gold = 100;
+            _player.Level = 1;
+            _player.Experience = 0;
+
+            lblHitPoints.Text = _player.CurrentHp.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblLevel.Text = _player.Level.ToString();
+            lblExperience.Text = _player.Experience.ToString();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -29,10 +45,7 @@ namespace RPGGAME2
 
         private void btnTest_Click(object sender, EventArgs e)
         {
-            lblHitPoints.Text = "100";
-            lblGold.Text = "100";
-            lblLevel.Text = "1";
-            lblExperience.Text = "0";
+          
             btnStart.Hide();
         }
 
@@ -50,5 +63,8 @@ namespace RPGGAME2
         {
 
         }
+        
+       
+        
     }
 }
---
+++ RPGGAME2/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGGAME2
+{
+    internal class Player
+    {
+        public int MaxHp { get; set; }
+        public int CurrentHp { get; set; }
+        public int Gold { get; set; }
+        public int Experience { get; set; }
+        public int Level { get; set; }
+
+        
+    }
+}
