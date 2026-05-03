4d3cc9f90127a48d775fbbb8f701e25fe4c135ef
YazanAlAraaj <yazanxkinge@gmail.com>
2026-05-03 23:57:37 +0300

    Finished the Move system Added a system to spawn in monster (Took forever to figure out) Fixed an error Removed some over commenting

--- RPGGAME2/Form1.cs
+++ RPGGAME2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -21,8 +22,9 @@ namespace RPGGAME2
         public rpggame1()
         {
             InitializeComponent();
-            _player = new Player(100, 0, 1, 100, 100);
-            MoveTo(World.Location_ID_Home);
+            _player = new Player(100, 0, 1, 10, 10);
+            MoveTo(World.LocationByID(World.Location_ID_Home));
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.Item_ID_Rusty_Sword), 1));
 
 
             lblHitPoints.Text = _player.CurrentHP.ToString();
@@ -81,22 +83,22 @@ namespace RPGGAME2
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToNorth);
         }
 
         private void btnEast_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToEast);
         }
 
         private void btnWest_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToWest);
         }
 
         private void btnSouth_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToSouth);
         }
 
         private void MoveTo(Location newLocation)
@@ -116,6 +118,7 @@ namespace RPGGAME2
                     return;
                 }
             }
+            
             _player.CurrentLocation = newLocation;
 
             btnNorth.Visible = (newLocation.LocationToNorth != null);
@@ -153,17 +156,17 @@ namespace RPGGAME2
                     {
                         bool PlayerHasAllItemToCompleteQuest = true;
 
-                        foreach(QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompeltionItems)
+                        foreach (QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompeltionItems)
                         {
-                            bool FoundItemInPlayersInventory=false;
-                            
-                            foreach(InventoryItem II in _player.Inventory)
+                            bool FoundItemInPlayersInventory = false;
+
+                            foreach (InventoryItem II in _player.Inventory)
                             {
                                 if (II.Details.ID == qci.Details.ID)
                                 {
                                     FoundItemInPlayersInventory = true;
 
-                                    if(II.Quantity < qci.Quantity)
+                                    if (II.Quantity < qci.Quantity)
                                     {
                                         PlayerHasAllItemToCompleteQuest = false;
                                         break;
@@ -178,14 +181,14 @@ namespace RPGGAME2
                                 break;
                             }
                         }
-                        if(PlayerHasAllItemToCompleteQuest)
+                        if (PlayerHasAllItemToCompleteQuest)
                         {
                             rtbMessage.Text += Environment.NewLine;
                             rtbMessage.Text += "You complete the '" + newLocation.QuestAvailableHere.Name + "' quest." + Environment.NewLine;
                         }
                         foreach (QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompeltionItems)
                         {
-                            foreach(InventoryItem II in _player.Inventory)
+                            foreach (InventoryItem II in _player.Inventory)
                             {
                                 if (II.Details.ID == qci.Details.ID)
                                 {
@@ -204,17 +207,182 @@ namespace RPGGAME2
                         _player.Experience += newLocation.QuestAvailableHere.RewardedXP;
                         _player.Gold += newLocation.QuestAvailableHere.RewardedGold;
 
+                        bool AddedItemToPlayerInventory = false;
+
+                        foreach (InventoryItem II in _player.Inventory)
+                        {
+                            if (II.Details.ID == newLocation.QuestAvailableHere.RewardItem.ID)
+                            {
+                                II.Quantity++;
+
+                                AddedItemToPlayerInventory = false;
+
+                                break;
+                            }
+                        }
+
+                        if (!AddedItemToPlayerInventory)
+                        {
+                            _player.Inventory.Add(new InventoryItem(newLocation.QuestAvailableHere.RewardItem, 1));
+                        }
+
+                        foreach (PlayerQuest pq in _player.PlayerQuests)
+                        {
+                            if (pq.Details.ID == newLocation.QuestAvailableHere.ID)
+                            {
+                                pq.IsCompleted = true;
+
+                                break;
+
+                            }
+                        }
 
 
                     }
                 }
 
             }
+            else
+            {
+                rtbMessage.Text += "You Recieve The" + newLocation.QuestAvailableHere.Name + "Quest" + Environment.NewLine;
+                rtbMessage.Text += newLocation.QuestAvailableHere.Description;
+                rtbMessage.Text += "To complete it return with: " + Environment.NewLine;
 
+                foreach (QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompeltionItems)
+                {
+                    if (qci.Quantity == 1)
+                    {
+                        rtbMessage.Text += qci.Quantity.ToString() + " " + qci.Details.Name + Environment.NewLine;
+                    }
+                    else
+                    {
+
+                        rtbMessage.Text += qci.Quantity.ToString() + " " + qci.Details.NamePlural + Environment.NewLine;
+                    }
+                }
+                rtbMessage.Text = Environment.NewLine;
+            }
+
+            if (newLocation.MonsterLivingHere != null)
+            {
+                rtbMessage.Text += "There's a:" + newLocation.MonsterLivingHere.Name + "here";
+
+                Monster StanderedMonster = World.MonsterByID(newLocation.MonsterLivingHere.ID);
+
+                _currentmonster = new Monster(StanderedMonster.ID, StanderedMonster.Name, StanderedMonster.MaxDamage, StanderedMonster.RewardedXP, StanderedMonster.RewardedGold, StanderedMonster.CurrentHP, StanderedMonster.MaxHP);
+
+                foreach (LootItem li in StanderedMonster.LootTable)
+                {
+                    _currentmonster.LootTable.Add(li);
+                }
+
+                cboWeapons.Visible = true;
+                cboPotions.Visible = true;
+                btnUseWeapon.Visible = true;
+                btnUsePotion.Visible = true;
+            }
+            else
+            {
+                _currentmonster = null;
+
+                cboWeapons.Visible = false;
+                cboPotions.Visible = false;
+                btnUseWeapon.Visible = false;
+                btnUsePotion.Visible = false;
+
+
+            }
+
+            dgvInventory.RowHeadersVisible = false;
+
+            dgvInventory.ColumnCount = 2;
+            dgvInventory.Columns[0].Name = "Name";
+            dgvInventory.Columns[0].Width = 197;
+            dgvInventory.Columns[1].Name = "Quantity";
+
+
+            dgvInventory.Rows.Clear();
+
+            foreach (InventoryItem ii in _player.Inventory)
+            {
+                if (ii.Quantity > 0)
+                {
+                    dgvInventory.Rows.Add(new[] { ii.Details.Name, ii.Quantity.ToString() });
+                }
+
+            }
+
+            dgvQuests.RowHeadersVisible = false;
+
+            dgvQuests.ColumnCount = 2;
+            dgvQuests.Columns[0].Name = "Name";
+            dgvQuests.Columns[0].Width = 197;
+            dgvQuests.Columns[1].Name = "Done?";
+
+            dgvQuests.Rows.Clear();
+
+            foreach (PlayerQuest pq in _player.PlayerQuests)
+            {
+                dgvQuests.Rows.Add(new[] { pq.Details.Name, pq.IsCompleted.ToString() });
+            }
+
+            List<Weapon> weapon = new List<Weapon>();
+
+            foreach (InventoryItem ii in _player.Inventory)
+            {
+                if (ii.Details is Weapon)
+                {
+                    if (ii.Quantity > 0)
+                    {
+                        weapon.Add((Weapon)ii.Details);
+                    }
+                }
+            }
+            if (weapon.Count == 0)
+            {
+                cboWeapons.Visible = false;
+                btnUseWeapon.Visible = false;
+            }
+            else
+            {
+                cboWeapons.DataSource = weapon;
+                cboWeapons.DisplayMember = "Name";
+                cboWeapons.ValueMember = "ID";
+
+                cboWeapons.SelectedIndex = 0;
+            }
+
+            List<HealingPotion> healingpotion = new List<HealingPotion>();
+
+            foreach (InventoryItem ii in _player.Inventory)
+            {
+                if (ii.Details is HealingPotion)
+                {
+                    if (ii.Quantity > 0)
+                    {
+                        healingpotion.Add((HealingPotion)ii.Details);
+                    }
+                }
+            }
+
+            if(healingpotion.Count == 0)
+            {
+                cboPotions.Visible = false;
+                btnUsePotion.Visible = false;
+            }
+            else
+            {
+                cboPotions.DataSource = healingpotion;
+                cboPotions.DisplayMember = "Name";
+                cboPotions.ValueMember = "ID";
+
+                cboPotions.SelectedIndex = 0;
+            }
 
 
 
         }
     }
 }
+
 
--- RPGGAME2/World.cs
+++ RPGGAME2/World.cs
@@ -12,31 +12,31 @@ namespace RPGGAME2
         public static List<Monster> Monsters = new List<Monster>();
         public static List<Quest> Quests = new List<Quest>();
         public static List<Location> Locations = new List<Location>();
-        //--------------------------------------------------------------------------------------------------------------------
+        
         //items start
         public const int Item_ID_Rusty_Sword = 1;
         public const int Item_ID_Rat_Tail = 2;
         public const int Item_ID_Healing_Potion = 3;
         public const int Item_ID_Warrior_Sword = 4;
         //items end
-        //--------------------------------------------------------------------------------------------------------------------
+        
         //locations start
         public const int Location_ID_Home = 1;
         public const int Location_ID_TownSquare = 2;
         public const int Location_ID_GraveYard = 3;
 
         //locations end
-        //--------------------------------------------------------------------------------------------------------------------
+        
         //quests start
         public const int Quest_ID_Clear_TownSquare = 1;
         public const int Quest_ID_Kill_The_Warrior = 2;
         //quests end
-        //--------------------------------------------------------------------------------------------------------------------
+        
         //monsters start
         public const int Monster_ID_Rat = 1;
         public const int Monster_ID_Warrior = 2;
         //monsters end
-        //--------------------------------------------------------------------------------------------------------------------
+        
 
 
         private static void PopulateItems()
@@ -55,36 +55,36 @@ namespace RPGGAME2
         }
         private static void PopulateQuests()
         {
-            //---------------------------------------------------------------------------------- 
+            
             Quest ClearTownSquare = new Quest(Quest_ID_Clear_TownSquare, "Clear Town Square", "Kill the rat to clear the town square", 15, 10);
             ClearTownSquare.QuestCompeltionItems.Add(new QuestCompletionItem(ItemByID(Item_ID_Rat_Tail),1));
             ClearTownSquare.RewardItem = ItemByID(Item_ID_Healing_Potion);
-            //---------------------------------------------------------------------------------- 
+            
             Quest KillTheWarrior = new Quest(Quest_ID_Kill_The_Warrior, "Kill the warrior", "Kill the warrior to get his sword", 20, 20);
             KillTheWarrior.QuestCompeltionItems.Add(new QuestCompletionItem(ItemByID(Item_ID_Warrior_Sword), 1));
             KillTheWarrior.RewardItem = ItemByID(Item_ID_Healing_Potion);
-            //---------------------------------------------------------------------------------- 
+            
             Quests.Add(ClearTownSquare);
             Quests.Add(KillTheWarrior);
         }
 
         private static void PopulateLocations()
         {
-            //---------------------------------------------------------------------------------- //Declare locations first
+            //Declare locations first
             Location Home = new Location(Location_ID_Home, "Home", "This is your home");
             Location TownSquare = new Location(Location_ID_TownSquare, "Town Square", "Welcome to the town square you see a fountain");
             Location GraveYard = new Location(Location_ID_GraveYard, "Grave Yard", "This Grave Yard is guarded by a warrior!");
-            //---------------------------------------------------------------------------------- //Location headings 2nd
-            Home.LocationToNorth = TownSquare;
-            TownSquare.LocationToSouth = Home;
-            TownSquare.LocationToNorth = GraveYard;
-            GraveYard.LocationToSouth = TownSquare;
-            //---------------------------------------------------------------------------------- //Monsters in location 3rd
+            //Location headings 2nd
+            Home.LocationToNorth = LocationByID(Location_ID_TownSquare);
+            TownSquare.LocationToSouth = LocationByID(Location_ID_Home);
+            TownSquare.LocationToNorth = LocationByID(Location_ID_GraveYard);
+            GraveYard.LocationToSouth = LocationByID(Location_ID_TownSquare);
+            //Monsters in location 3rd
             TownSquare.MonsterLivingHere = MonsterByID(Monster_ID_Rat);
             TownSquare.QuestAvailableHere = QuestByID(Quest_ID_Clear_TownSquare);
             GraveYard.MonsterLivingHere = MonsterByID(Monster_ID_Warrior);
             GraveYard.QuestAvailableHere = QuestByID(Quest_ID_Kill_The_Warrior);
-            //---------------------------------------------------------------------------------- //Then we ADD them
+            //Then we ADD them
             Locations.Add(Home);
             Locations.Add(TownSquare);
             Locations.Add(GraveYard);
@@ -93,13 +93,13 @@ namespace RPGGAME2
 
         private static void PopulateMonsters()
         {
-            //---------------------------------------------------------------------------------- // declare the Monster first and give it the parametrs
+           // declare the Monster first and give it the parametrs
             Monster rat = new Monster(Monster_ID_Rat, "Rat", 2, 5, 5, 5, 5);
             Monster Warrior = new Monster(Monster_ID_Warrior,"Warrior", 10, 20, 10, 10, 10);
-            //---------------------------------------------------------------------------------- // add the loot
+            // add the loot
             rat.LootTable.Add(new LootItem(ItemByID(Item_ID_Rat_Tail), 100, true));
             Warrior.LootTable.Add(new LootItem(ItemByID(Item_ID_Warrior_Sword), 100, true));
-            //---------------------------------------------------------------------------------- // Add to list
+           // Add to list
             Monsters.Add(rat);
             Monsters.Add(Warrior);
         }
@@ -131,6 +131,16 @@ namespace RPGGAME2
             {
                 if (monster.ID == ID)
                     return monster;
+            }
+            return null;
+        }
+
+        public static Location LocationByID(int ID)
+        {
+            foreach (Location location in Locations)
+            {
+                if (location.ID == ID) 
+                    return location;
             }
             return null;
         }
